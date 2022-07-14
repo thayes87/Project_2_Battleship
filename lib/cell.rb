b04a99dc09c09ef2942bdd350408fd97ad6c5f66
Mary Ballantyne <mballantyne777@gmail.com>
2022-07-13 19:56:34 -0600

    add render method

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,7 +1,8 @@
 class Cell
-  attr_reader :coordinate, :ship
+  attr_reader :coordinate, :ship, :render
   def initialize(coordinate)
     @coordinate = coordinate
+    @fired_upon = false
   end
 
   def empty?
@@ -11,6 +12,23 @@ class Cell
   def place_ship(ship)
     @ship = ship
 
+  end
+
+  def fired_upon?
+    @fired_upon 
+  end
+
+  def fire_upon
+    @ship.hit if @ship
+    @fired_upon = true
+  end
+
+  def render()
+    if fired_upon? == false
+      "."
+    elsif fired_upon? == true && empty? == true
+      "M"
+    end
   end
 
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -20,5 +20,49 @@ RSpec.describe Cell do
 
     expect(cell.ship).to be(cruiser)
     expect(cell.empty?).to be(false)
+    expect(cell.fired_upon?).to be(false)  
   end 
+  
+  it 'can take a hit' do
+    cruiser = Ship.new("Cruiser", 3)
+    cell = Cell.new("B4")
+    
+    cell.place_ship(cruiser)
+
+    cell.fire_upon
+
+    expect(cell.ship.health).to eq(2)
+    expect(cell.fired_upon?).to be(true)
+  end
+
+  it 'can render empty cell' do
+    cell_1 = Cell.new("B4")
+    cell_1.render
+
+    expect(cell_1.render).to eq(".")
+
+    cell_1.fire_upon
+    cell_1.render
+    expect(cell_1.render).to eq("M")
+  end
+  
+  it 'can render cell with ship' do
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell_2.place_ship(cruiser)
+    cell_2.render
+
+    expect(cell_2.render).to eq(".")
+  end
+
+  it 'can render an optional argument' do
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell_2.place_ship(cruiser)
+    cell_2.render(true)
+
+    expect(cell_2.render(true)).to eq("S")
+  end
 end
