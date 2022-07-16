c7bf2eb6ff87ff5fed0a111e6ace9d6ef862eab1
Mary Ballantyne <mballantyne777@gmail.com>
2022-07-15 18:39:47 -0600

    fix validate ship placement

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,7 @@
 class Board
   attr_reader :cells 
-  def initialize
 
+  def initialize
     @cells = {}
 
     ("A".."D").each do |letter|
@@ -22,15 +22,12 @@ class Board
   end
 
   def valid_placement?(ship, coordinates)
-    coordinates.count == ship.length
-    consecutive_number?(coordinates)
-  end
-
-  def consecutive_placement?(ship, coordinates)
+    return false if coordinates.count != ship.length
     consecutive_number?(coordinates)
   end
 
   def consecutive_number?(coordinates)
+    pp coordinate_hash(coordinates)
     coordinate_hash(coordinates)[:number].each_cons(2).all? do |num_1, num_2|
       num_1.to_i == num_2.to_i - 1
     end
@@ -42,12 +39,13 @@ class Board
       hash[:letter] << coord[0]
       hash[:number] << coord[1]
     end
+    hash
   end
 
   def consecutive_letter?
     coordinate_hash(coordinates)[:letter].each_cons(2).all? do |letter_1, letter_2|
       letter_1.ord == letter_2.ord - 1
+    end
   end
-end
 end
  
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -36,12 +36,15 @@ RSpec.describe Board do
   end
   
   it 'validates ship placement' do
+    expect(board.valid_placement?(cruiser, ["A1", "A2", "A3"])).to be true
+
     expect(board.valid_placement?(cruiser, ["A1", "A2"])).to be false
     expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be false
     expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be false
     expect(board.valid_placement?(submarine, ["A1", "C1"])).to be false
     expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be false
     expect(board.valid_placement?(submarine, ["C1", "B1"])).to be false
+    expect(board.valid_placement?(cruiser, ["A1", "A2", "B2"])).to be false
   end
 
   
