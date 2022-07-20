bb5d2a5827830a62aab2823ea5ed42c2fd4f26aa
Mary Ballantyne <mballantyne777@gmail.com>
2022-07-20 15:29:30 -0600

    simplify valid_coordinate?

--- lib/board.rb
+++ lib/board.rb
@@ -17,11 +17,7 @@ class Board
 
   def valid_coordinate?(coordinate)
     location = cells[coordinate]
-     if location && location.coordinate == coordinate
-      true
-    else 
-      false
-    end
+    location&.coordinate == coordinate
   end
 
   def valid_placement?(ship, coordinates)
